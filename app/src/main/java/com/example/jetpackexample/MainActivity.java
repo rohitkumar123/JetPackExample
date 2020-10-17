d7d25bb50f32ce5a427319d07aa1b423bb32cc0d
rohitkumar31 <rohit.kumar31@dxc.com>
2020-10-17 18:23:14 +0530

    removed unused code

--- app/src/main/java/com/example/jetpackexample/MainActivity.java
+++ app/src/main/java/com/example/jetpackexample/MainActivity.java
@@ -10,7 +10,7 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
+      //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
