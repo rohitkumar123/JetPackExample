6d34cbd917a6d02c8375b1402c8ab5cb377e755d
rohitkumar31 <rohit.kumar31@dxc.com>
2020-10-20 14:30:21 +0530

    added entity file

---
+++ app/src/main/java/com/example/jetpackexample/room/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.jetpackexample.room.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "FirstName")
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Ignore
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
